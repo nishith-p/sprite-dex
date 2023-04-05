3911b1a65a247f3da5c5a3cafd3f9208bf3b5877
Nishith <IT18140576@my.sliit.lk>
2023-04-05 18:23:19 +0530

    Updated to src/app, added para comp

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Paragraph from "@/app/components/ui/Paragraph";
+
+export default function Home() {
+  return (
+    <div>
+      <Paragraph size="sm">Hello World!</Paragraph>
+    </div>
+  );
+}
